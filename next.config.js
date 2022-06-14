8d15c8973d5dbfb5f2dd4d97e7562abd7b7865f2
vaib1343 <vaib1343@gmail.com>
2022-06-14 21:53:38 +0530

    more config changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
+    reactStrictMode: false,
     env: {
         DB_LOCAL_URI: 'mongodb+srv://vaib1343:1343@cluster0.nyght.mongodb.net/?retryWrites=true&w=majority',
         CLOUDINARY_CLOUD_NAME: 'dmgsaurar',
